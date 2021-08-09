d3a805e732ba4fb2efb90c6296aa7135ec2d6755
Blazzike <blazzike@gmail.com>
2021-08-09 16:34:06 +0100

    Allow multiple react components per file

--- rules/react/base.js
+++ rules/react/base.js
@@ -171,7 +171,7 @@ module.exports = {
      *
      * Prevent multiple component definition per file
      */
-    'react/no-multi-comp': 'error',
+    'react/no-multi-comp': 'off',
 
     /*
      * https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-redundant-should-component-update.md
